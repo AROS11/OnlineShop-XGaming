aa652eb6dd7e4b94a7984de2793807dc1ade6f6c
AROS11 <ao56171@ubt-uni.net>
2024-01-18 18:03:41 +0200

    OnlineShop

---
+++ Online-Shop/Data/ApplicationDbContext.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Online_Shop.Models;
+
+namespace Online_Shop.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        private readonly IHttpContextAccessor httpContextAccessor;
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IHttpContextAccessor httpContextAccessor)
+            : base(options)
+        {
+            this.httpContextAccessor = httpContextAccessor;
+        }
+        
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetails> OrderDetails { get; set; }
+
+        public async Task<int> SaveChangesAsync() 
+        {
+            string user = httpContextAccessor.HttpContext.User.Identity.Name;
+            AddTimestamps(user);
+            return await base.SaveChangesAsync();
+        }
+        private void AddTimestamps(string owner)
+        {
+            foreach (EntityEntry<BaseEntity> entry in ChangeTracker.Entries<BaseEntity>()) 
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedBy = owner;
+                        entry.Entity.CreatedDate = DateTime.Now;
+                        break;
+
+                    case EntityState.Modified:
+                        entry.Entity.UpdatedBy = owner;
+                        entry.Entity.UpdatedDate = DateTime.Now;
+                        break;
+                }
+            }
+        }
+       
+        public DbSet<Online_Shop.Models.Contact> Contact { get; set; }
+        public DbSet<Online_Shop.Models.Product> Product { get; set; }
+    }
+}
